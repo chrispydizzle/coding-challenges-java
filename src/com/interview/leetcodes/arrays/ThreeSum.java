bd5ae57997270608d36aca564486a67e12ffe49d
cp <chris@cpsharp.net>
2020-02-05 01:28:24 -0500

    Still fighting with ThreeSum, need to rewrite.

--- src/com/interview/leetcodes/arrays/ThreeSum.java
+++ src/com/interview/leetcodes/arrays/ThreeSum.java
@@ -8,51 +8,51 @@ import java.util.List;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum (@NotNull int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        List<Integer> untouched = Arrays.stream(nums)
-                                        .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-
-        List<Integer> diffOfTarget = Arrays.stream(nums)
-                                           .map(i -> -i)
-                                           .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        List<Integer> untouched = Arrays.stream(nums).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
         // now it's just twosum to make numbers that exist in nums, and we add back in the base numbers later.
+        List<int[]> triedCombos = new ArrayList<>();
+        List<Integer> oldList = new ArrayList<>(untouched);
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                List<Integer> oldList = Arrays.stream(nums)
-                                              .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-                List<Integer> tempList = new ArrayList<>(diffOfTarget);
-                int newValue = nums[i] + nums[j];
+            int num1 = oldList.get(i);
+            int num2 = oldList.get(i + 1);
 
-                List<Integer> potentials = new ArrayList<>();
-                potentials.add(nums[i]);
-                potentials.add(nums[j]);
+            if (triedCombos.size() > 0) {
+                if (num1 < num2 && triedCombos.stream().anyMatch(m -> m[0] == num1 && m[1] == num2)) {
+                    continue;
+                } else if (num1 < num2 && triedCombos.stream().anyMatch(m -> m[0] == num2 && m[1] == num1)) {
+                    continue;
+                }
+            }
+            List<Integer> potentials = new ArrayList<>();
+            potentials.add(num1);
+            potentials.add(num2);
 
-                tempList.remove(i);
-                tempList.remove(i);
-                oldList.remove(i);
-                oldList.remove(i);
-
-                while (oldList.contains(-newValue)) {
-                    int windex = oldList.indexOf(-newValue);
-
-                    potentials.add(-newValue);
-                    tempList.remove(windex);
+            oldList.remove(0);
+            while (oldList.size() > 1) {
+                int newValue = num1 + num2;
+                oldList.remove(0);
+                potentials.add(-newValue);
+                potentials.sort(Integer::compareTo);
+                List<Integer> finalPotentials = potentials;
+                while (oldList.contains(-newValue) && result.stream().noneMatch(r -> r.get(0).equals(finalPotentials.get(0)) && r.get(1).equals(finalPotentials.get(1)) && r.get(2).equals(finalPotentials.get(2)))) {
+                    int windex = oldList.lastIndexOf(-newValue);
                     oldList.remove(windex);
-                    potentials.sort(Integer::compareTo);
-                    int uindex = untouched.indexOf(-newValue);
-                    List<Integer> finalPotentials = potentials;
-                    if (result.stream()
-                              .noneMatch(integers -> integers.containsAll(finalPotentials)) && uindex != i && uindex != j) {
+                    int uindex = untouched.lastIndexOf(-newValue);
+                    if (uindex != i && uindex != i + 1) {
                         result.add(potentials);
+                    } else {
+                        triedCombos.add(new int[]{potentials.get(0), potentials.get(1)});
                     }
 
                     potentials = new ArrayList<>();
                     potentials.add(nums[i]);
-                    potentials.add(nums[j]);
+                    potentials.add(nums[i + 1]);
                 }
             }
+            oldList = new ArrayList<>(untouched).subList(i);
         }
 
         return result;
